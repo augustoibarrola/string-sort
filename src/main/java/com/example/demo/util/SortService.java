83c05dedd46684249b583b92e51def5d63b9196c
augustoibarrola <augusto.t.ibarrola@gmail.com>
2023-10-06 18:33:28 -0400

    able to read from excel file

--- src/main/java/com/example/demo/util/SortService.java
+++ src/main/java/com/example/demo/util/SortService.java
@@ -1,5 +1,7 @@
 package com.example.demo.util;
 
+import java.io.InputStream;
+
 import com.aspose.cells.Workbook;
 import com.aspose.cells.Worksheet;
 import com.aspose.cells.WorksheetCollection;
@@ -13,22 +15,47 @@ public class SortService {
 	
 	public void duh() {
 	
-		/*
-		 * Load the desired workbook file
-		 */
-		Workbook workbook = new Workbook();
-		
-		/*
-		 * Get all worksheets
-		 */
-		WorksheetCollection collection = workbook.getWorksheets();
 
-		Worksheet worksheet = collection.get(0);
-		
-		  System.out.print("Worksheet: " + worksheet.getName());
+		// Load the desired workbook file
+		InputStream is = getClass().getClassLoader().getResourceAsStream("AUGUSTO_GOODREADS_FINAL.xlsx");
 
 		
-		System.out.print("Worksheet: " + worksheet.getName());
+		
+		try {
+			Workbook workbook = new Workbook(is);
+			
+			// get all the worksheets in the loaded workbook
+			WorksheetCollection worksheets = workbook.getWorksheets();
+			
+			loopThroughWorksheets(worksheets);
+			
+			
+
+			
+		} catch (Exception e) {
+			System.out.println("something failed");
+			e.printStackTrace();
+		}
+		
+
+
+		
+	}
+	
+	public void loopThroughWorksheets(WorksheetCollection worksheets) {
+		
+		
+		for (int sheetIndex = 0; sheetIndex < worksheets.getCount(); sheetIndex++) {
+			
+			Worksheet worksheet = worksheets.get(sheetIndex);
+			
+			System.out.println("WORKSHEET NAME : "+ worksheet.getName());
+			
+			int worksheetRowCount = worksheet.getCells().getMaxDataRow();
+			
+			System.out.print(worksheetRowCount);
+			
+		}
 		
 	}
 	
