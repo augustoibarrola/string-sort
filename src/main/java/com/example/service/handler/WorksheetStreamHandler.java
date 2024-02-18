3a31d2fd6b6ef23c217454c2aa2dee43087863e6
augustoibarrola <augusto.t.ibarrola@gmail.com>
2024-02-18 01:49:34 -0500

    code cleanup

--- src/main/java/com/example/service/handler/WorksheetStreamHandler.java
+++ src/main/java/com/example/service/handler/WorksheetStreamHandler.java
@@ -5,48 +5,44 @@ import java.io.InputStream;
 
 import com.aspose.cells.Workbook;
 import com.aspose.cells.WorksheetCollection;
-import com.example.service.reader.FileReaderService;
 
 public class WorksheetStreamHandler {
 
     private InputStream worksheetStream;
 
-    public WorksheetStreamHandler(){};
+    public WorksheetStreamHandler() {
+    };
 
-    public WorksheetStreamHandler(String fileLOcation){
+    public WorksheetStreamHandler(String fileLOcation) {
         this.worksheetStream = openInputStreamOfFile(fileLOcation);
         System.out.println("Worksheet Stream Opened");
     }
 
-    public InputStream openInputStreamOfFile(String fileLocation) { 
+    public InputStream openInputStreamOfFile(String fileLocation) {
         return getClass().getClassLoader().getResourceAsStream(fileLocation);
-   }
-    
-   public WorksheetCollection loadWorksheetCollectionFromInputStream() {	
-    try {
-        System.out.println("Getting Worksheet from Stream...");
-        Workbook workbook = new Workbook(this.worksheetStream);
-        WorksheetCollection worksheets = workbook.getWorksheets(); 
-        return worksheets;
-        
-    } catch (Exception e) {
-        System.out.println("something failed");
-        e.printStackTrace();
-        return null;
     }
-    
-    
-}
-public void closeInputStream() {
-    try {
-        this.worksheetStream.close();
-    } catch (IOException e) {
-        e.printStackTrace();
+
+    public WorksheetCollection loadWorksheetCollectionFromInputStream() {
+        try {
+            System.out.println("Getting Worksheet from Stream...");
+            Workbook workbook = new Workbook(this.worksheetStream);
+            WorksheetCollection worksheets = workbook.getWorksheets();
+            return worksheets;
+
+        } catch (Exception e) {
+            System.out.println("something failed");
+            e.printStackTrace();
+            return null;
+        }
+
     }
-}
-    
 
-
-
+    public void closeInputStream() {
+        try {
+            this.worksheetStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
