0b473601c44cf1ad2b7a91e3d22f162509727562
augustoibarrola <augusto.t.ibarrola@gmail.com>
2024-02-20 11:32:07 -0500

    getting book titles and their authors

--- src/main/java/com/example/service/reader/FileReaderService.java
+++ src/main/java/com/example/service/reader/FileReaderService.java
@@ -33,5 +33,33 @@ public class FileReaderService extends WorksheetUtilities{
 
 	public void getAllBooksAndTheirBookshelves(Worksheet worksheet){
 		System.out.println("Getting all Book Titles and their Bookshelves...");
+		
+		//1. find the index of the Book Title Column
+		Integer bookTitleColumn = getColumnHeaderBookTitle(worksheet);
+		//TODO - this is just for now; logic should be fleshed out to get author column when index isnt known
+		Integer bookAuthorColumn = 2;
+		
+		//2. find the max number of books in the sheet
+		Integer maxNumberOfRowsWithData = getMaxNumberOfRows(worksheet);
+		
+		//loop down the worksheet
+		// we're starting at 1 since 0 would be 
+		// row with the column headers. 
+		for(int row = 1; row <= maxNumberOfRowsWithData; row++){
+			
+			String bookTitle = getBookTitle(worksheet, row, bookTitleColumn);
+			String bookAuthor = getBookAuthor(worksheet, row, bookAuthorColumn);
+			
+			System.out.println(bookTitle +" by "+bookAuthor);
+		}
 	}
+
+	public String getBookTitle(Worksheet worksheet, Integer row, Integer column){
+		return worksheet.getCells().get(row, column).getStringValue();
+	}
+
+	public String getBookAuthor(Worksheet worksheet, Integer row, Integer column){
+		return worksheet.getCells().get(row, column).getStringValue();
+	}
+
 }
--- src/main/java/com/example/util/WorksheetUtilities.java
+++ src/main/java/com/example/util/WorksheetUtilities.java
@@ -10,4 +10,11 @@ public abstract class WorksheetUtilities {
 	}
 
 	public abstract Integer getMaxNumberOfRows(Worksheet worksheet);
+
+	protected Integer getColumnHeaderBookTitle(Worksheet worksheet){
+		// this is just for now; the logic should be 
+		// fleshed out for situations in which
+		// the index of 'Title' is not known. 
+		return 1 ;
+	}
 }
